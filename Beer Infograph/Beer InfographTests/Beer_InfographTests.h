99dae5b2cf57d1a39249aa1ec8f2bf64e18121e2
Roberto <rjo218@Osos-MacBook-Pro.local>
2012-06-04 14:49:50 -0400

    ...

---
+++ Beer Infograph/Beer InfographTests/Beer_InfographTests.h
@@ -0,0 +1,13 @@
+//
+//  Beer_InfographTests.h
+//  Beer InfographTests
+//
+//  Created by Roberto Osorio on 6/4/12.
+//  Copyright (c) 2012 Malloc Media L.L.C. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Beer_InfographTests : SenTestCase
+
+@end
---
+++ Beer Infograph/Beer InfographTests/Beer_InfographTests.m
@@ -0,0 +1,32 @@
+//
+//  Beer_InfographTests.m
+//  Beer InfographTests
+//
+//  Created by Roberto Osorio on 6/4/12.
+//  Copyright (c) 2012 Malloc Media L.L.C. All rights reserved.
+//
+
+#import "Beer_InfographTests.h"
+
+@implementation Beer_InfographTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Beer InfographTests");
+}
+
+@end
