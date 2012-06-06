ae07a20014655c5ad3efb68394b3f9cb5c3798fe
Pete Jelliffe <pete.jelliffe@gmail.com>
2012-06-06 08:16:04 -0400

    added a name, and added login to the page

--- brewnique.php
+++ brewnique.php
@@ -9,14 +9,9 @@ include 'Awsm/Service/Untappd.php';
 
 <?php
 $apiKey   = '68fc36c4d9e498293f8edfe6ab9453a2';
-<<<<<<< HEAD
-$username = '';
-$password = '';
-=======
 //use the username and passowrd passed via POST submission
 $username= (isset($_POST['username']) ? $_POST['username'] : '');
 $password= (isset($_POST['password']) ? $_POST['password'] : '');
->>>>>>> added a name, and added login to the page
         
 $untappd = new Awsm_Service_Untappd($apiKey, $username, $password);
         
@@ -24,18 +19,30 @@ $untappd = new Awsm_Service_Untappd($apiKey, $username, $password);
 if ($username != '') {       
 	//pull the list of distinct beers
 	
-	try {
-	    $result = $untappd->userDistinctBeers();
-	} catch (Awsm_Service_Untappd_Exception $e) {
-	    die($e->getMessage());
-	}
-
-  //create the list of beers
+	$offset=0;
+	$loop=1;
 	$autocomplete="[";
 	
-	foreach ($result->results as $value) {
-		$autocomplete.= "\"".$value->beer_name . " (" . $value->brewery_name . ")\",\n";
-	} 
+	//loop through API cals until you go through the whole list
+	while ($loop=25) {
+		try {
+		    $result = $untappd->userDistinctBeers($username, $offset);
+		} catch (Awsm_Service_Untappd_Exception $e) {
+		    die($e->getMessage());
+		}
+		
+		//increment the offset
+		$offset+=$result->returned_results;
+		
+		//stop looping if less than 25 results returned 
+		$loop=$result->returned_results;
+		
+	  //append to the list of beers
+		foreach ($result->results as $value) {
+			$autocomplete.= "\"".$value->beer_name . " (" . $value->brewery_name . ")\",\n";
+		} 
+	}
+	//close the array
 	$autocomplete.= "\"\"]";
 	
 }
@@ -43,6 +50,8 @@ if ($username != '') {
 //$result="{\"hi\":\"hello\"}";
 ?>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
 		<link type="text/css" href="css/ui-lightness/jquery-ui-1.8.21.custom.css" rel="stylesheet" />
 		<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
 		<script type="text/javascript" src="js/jquery-ui-1.8.21.custom.min.js"></script>
@@ -78,7 +87,7 @@ beerlist.sort();
 <h3>Brewnique helps you find beers you've already had, so you can make sure you're trying a new one!</h3>
 <p>Sign in with your Untappd username and password.  Don't worry, we don't store it.</p>
   <form action="" method="post">
-    <label for="username">Username:</label><input type="text" name="username" id="username" placeholder="Untappd username"> <br />
+    <label for="username">Username:</label><input type="text" name="username" id="username" placeholder="Untappd username" value="petroleumjelliffe"> <br />
     <label for="password">Password:</label><input type="password" name="password" id="password" placeholder="Untappd password"> <br/>
     
     <input type="submit" value="Sign in">
