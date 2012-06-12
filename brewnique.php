1baea043027e3b209bba9b037c18d30e9db34f32
Pete Jelliffe <pete.jelliffe@gmail.com>
2012-06-12 00:54:41 -0400

    loop through distinct calls until all uniques are added to array.

--- brewnique.php
+++ brewnique.php
@@ -4,6 +4,8 @@
 
 include 'Awsm/Service/Untappd.php';
 
+$debug=false; //set to true to use a static JS array and not call teh API to avoid the rate limit.
+
 ?>
 
 
@@ -12,6 +14,8 @@ $apiKey   = '68fc36c4d9e498293f8edfe6ab9453a2';
 //use the username and passowrd passed via POST submission
 $username= (isset($_POST['username']) ? $_POST['username'] : '');
 $password= (isset($_POST['password']) ? $_POST['password'] : '');
+        
+        
         
 $untappd = new Awsm_Service_Untappd($apiKey, $username, $password);
         
@@ -24,41 +28,79 @@ if ($username != '') {
 	$autocomplete="[";
 	$passes=1;
 
+	if ($debug) {
+		$autocomplete.="\"Thunderbolt (Wandering Star Brewing Company)\",
+										\"Slyfox Maibock (Sly Fox Brewing Company)\",
+										\"Lake Erie Monster (Great Lakes Brewing Company )\",
+										\"Edmund Fitzgerald (Great Lakes Brewing Company )\",
+										\"Holy Moses White Ale (Great Lakes Brewing Company )\",
+										\"Dortmunder Gold (Great Lakes Brewing Company )\",
+										\"Maibock (Greenport Harbor Brewing Company)\",
+										\"He'Brew Hop Manna IPA (Shmaltz Brewing Company)\",
+										\"Excelsior! Fourteen (Ithaca Beer Company)\",
+										\"Linchpin White IPA (Green Flash Brewing Co.)\",
+										\"Warlord Imperial IPA (McNeill's Brewery)\",
+										\"Moo Thunder Stout (Butternuts Beer & Ale)\",
+										\"Scotch Ale (Rooster Fish Brewing)\",
+										\"Big TIPA (Greenport Harbor Brewing Company)\",
+										\"Waldo's Special Ale (AKA Waldo's 420) (Lagunitas Brewing Company)\",
+										\"Existent (Stillwater Artisanal Ales)\",
+										\"Tongue Buckler (Ballast Point Brewing Company)\",
+										\"30th Street Pale Ale (Green Flash Brewing Co.)\",
+										\"Drifter Pale Ale (Widmer Brothers Brewing Company)\",
+										\"Dragon's Milk (New Holland Brewing Company)\",
+										\"Export Ale (Shipyard Brewing Company)\",
+										\"Sneaky Pete (Laughing Dog Brewing)\",
+										\"Organic Porter (Eel River Brewing Co.)\",
+										\"Apollo (Sixpoint Brewery)\",
+										\"Heavy Seas Loose Cannon Hop³ Ale (Heavy Seas Beer)\",
+										\"Avalanche Amber (Breckenridge Brewery)\",
+										\"Big Daddy IPA (Speakeasy Ales and Lagers)\",
+										\"Happy Hops (Russian River Brewing Company)\",
+										\"Reality Czeck (Moonlight Brewing Company)\",
+										\"Big Sur Golden (English Ales Brewery)\",
+										\"Hofbräu Maibock (Staatliches Hofbräuhaus München)\",
+										\"XHP (Jupiter)\",
+										\"Jupiter Porter (Jupiter)\",
+										\"\"]";
+	} else {
 	
-	//loop through API cals until you go through the whole list
-	while ($loop==25) {
-		try {
-		    $result = $untappd->userDistinctBeers($username, $offset);
-		} catch (Awsm_Service_Untappd_Exception $e) {
-		    die($e->getMessage());
+		//loop through API cals until you go through the whole list
+		while ($loop==25) {
+			try {
+			    $result = $untappd->userDistinctBeers($username, $offset);
+			} catch (Awsm_Service_Untappd_Exception $e) {
+			    die($e->getMessage());
+			}
+	/*
+			$result= new stdClass;
+			$result->returned_results=25;
+			$result->returned_results= new stdClass;
+			$result->returned_results->beer_name="test name";
+			$result->returned_results->brewery_name="test name";
+	*/
+			
+			//increment the offset
+			$offset+=$result->returned_results;
+			
+			//stop looping if less than 25 results returned 
+			$loop=$result->returned_results;
+			
+		  //append to the list of beers
+			foreach ($result->results as $value) {
+				$autocomplete.= "\"".$value->beer_name . " (" . $value->brewery_name . ")\",\n";
+			} 
+	
+	/*
+			echo "offset= $offset\n";
+			echo"pass number $passes\n";
+			$passes++;
+	*/
 		}
-/*
-		$result= new stdClass;
-		$result->returned_results=25;
-		$result->returned_results= new stdClass;
-		$result->returned_results->beer_name="test name";
-		$result->returned_results->brewery_name="test name";
-*/
-		
-		//increment the offset
-		$offset+=$result->returned_results;
-		
-		//stop looping if less than 25 results returned 
-		$loop=$result->returned_results;
-		
-	  //append to the list of beers
-		foreach ($result->results as $value) {
-			$autocomplete.= "\"".$value->beer_name . " (" . $value->brewery_name . ")\",\n";
-		} 
-
-		echo "offset= $offset\n";
-		echo"pass number $passes\n";
-		$passes++;
-	}
-
-	//close the array
-	$autocomplete.= "\"\"]";
 	
+		//close the array
+		$autocomplete.= "\"\"]";
+	}
 }
 //$result=json_encode($result);
 //$result="{\"hi\":\"hello\"}";
@@ -74,7 +116,7 @@ if ($username != '') {
 //alert(beerlist[0]);
 //beerlist.sort();
 //alert(beerlist[0]);  
-//
+
 </script>
   
   	<script>
@@ -86,7 +128,8 @@ beerlist.sort();
 
 
 		$( "#beers" ).autocomplete({
-			source: beerlist
+			source: beerlist,
+			minLength: 2
 		});
 	});
 	</script>
@@ -100,7 +143,7 @@ beerlist.sort();
 <h3>Brewnique helps you find beers you've already had, so you can make sure you're trying a new one!</h3>
 <p>Sign in with your Untappd username and password.  Don't worry, we don't store it.</p>
   <form action="" method="post">
-    <label for="username">Username:</label><input type="text" name="username" id="username" placeholder="Untappd username" value="petroleumjelliffe"> <br />
+    <label for="username">Username:</label><input type="text" name="username" id="username" placeholder="Untappd username" value=""> <br />
     <label for="password">Password:</label><input type="password" name="password" id="password" placeholder="Untappd password"> <br/>
     
     <input type="submit" value="Sign in">
